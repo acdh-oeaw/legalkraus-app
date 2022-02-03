c9c4f24ee776707fd8fb817c0a95d5a938ef74ed
A-Ortner <a.ortner20.1@gmail.com>
2022-02-03 14:22:07 +0100

    added type of rs to params of function getObjectWithId in PMBService.js

--- src/services/PMBService.js
+++ src/services/PMBService.js
@@ -1,8 +1,8 @@
 //https://pmb.acdh.oeaw.ac.at/entity/${resourceId}/
 
-module.exports.getObjectWithId = async(resourceId, callback) => {
+module.exports.getObjectWithId = async(resourceId, type, callback) => {
     let id = resourceId.substring(4); //removes the leading '#pmb'
-    console.log(id);
+    console.log(type);
     const resp = await fetch(`https://pmb.acdh.oeaw.ac.at/entity/${id}/?format=json`)
     const data = await resp.json();
     return callback(data);
