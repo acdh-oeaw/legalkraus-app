ad58fb66a9203d5a58c1879ed261a8504bf0aaec
Barbara Krautgartner <barbara.krautgartner@oeaw.ac.at>
2022-05-01 20:50:58 +0200

    favicon, site title

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 module.exports = {
     runtimeCompiler: true,
-    publicPath: process.env.NODE_ENV === 'production' ? '/legalkraus-app/' : '/'
+    publicPath: process.env.NODE_ENV === 'production' ? '/legalkraus-app/' : '/',
+    pages: {
+      index: {
+        // entry for the page
+        entry: 'src/main.js',
+        title: 'Rechtsakten Karl Kraus',
+      },
+    }  
   }
