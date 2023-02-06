ef80f1f5a59429b31f46a5f260c471893983fe40
Barbara Krautgartner <barbara.krautgartner@oeaw.ac.at>
2023-02-06 09:59:53 +0000

    fix vocabs endpoint

--- src/services/VocabsService.js
+++ src/services/VocabsService.js
@@ -1,4 +1,4 @@
-const Vocabs_BaseUrl = 'https://vocabs.acdh.oeaw.ac.at/rest/v1/legalkraus';
+const Vocabs_BaseUrl = 'https://vocabs.acdh.oeaw.ac.at/rest/v1/legal_kraus';
 const Vocabs_TopConceptsEndpoint = `${Vocabs_BaseUrl}/topConcepts`;
 //const Vocabs_DataEndpoint = `${Vocabs_BaseUrl}/data?format=application/json`;
 const Vocabs_ChildrenEndpoint = `${Vocabs_BaseUrl}/children?format=application/json`;
